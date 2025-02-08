e25b3d991502d576566d71def34cac5cb07d9c20
D4rkr34lm <darth.m.frohn@gmail.com>
2025-02-08 19:00:13 +0100

    Add formater, configure file explorer

--- init.lua
+++ init.lua
@@ -17,4 +17,9 @@ vim.opt.wrap = true -- wrap lines
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
 vim.cmd("Neotree")
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,6 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-	opts = {}
+  	"s1n7ax/nvim-window-picker"
+	},
+	opts = {
+		close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+    open_files_using_relative_paths = false,
+    sort_case_insensitive = false, -- used when sorting files and directories in the tree
+    sort_function = nil,
+	}
 }
